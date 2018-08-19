aa3b20a2cda30cce27da3af734f6334c5fab6f9b
HiromichiKato <cp3xzayn@icloud.com>
2018-08-19 18:39:50 +0900

    StepAsideUnityChanプロジェクト初めてのコミット
    unitychanが通り越したオブジェクトを破壊するようにしました。

---
+++ Assets/ItemDestroyer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour {
+    //unitychanを取得する
+    private GameObject unitychan;
+
+    // Use this for initialization
+    void Start(){
+        //unitychanを取得
+        this.unitychan = GameObject.Find("unitychan");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(this.unitychan.transform.position.z - this.gameObject.transform.position.z > 5) {
+            Destroy(this.gameObject);
+        }
+	}
+}
